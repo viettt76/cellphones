48e166fa4cab13abc6e02fb6d60bd2d3c1c324a1
viettt76 <viethq@deepsync.vn>
2025-12-03 08:53:11 +0700

    project initiation

---
+++ lib/constants.ts
@@ -0,0 +1,22 @@
+export const paths = {
+    home: '/',
+    login: '/login',
+    signup: '/signup',
+};
+
+export const languages = [
+    {
+        value: 'vi',
+        label: 'Tiếng Việt',
+        icon: '/images/flags/ic-flag-vi.svg',
+    },
+    {
+        value: 'en',
+        label: 'English',
+        icon: '/images/flags/ic-flag-en.svg',
+    },
+];
+
+export const envs = {
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+};
